2338bc7ee1d9036328e532897b920de4d302a0ec
minuk07 <lmw3164@naver.com>
2025-01-28 13:41:59 +0900

    Http Session 이용하여 로그인 서비스 구현

--- session/src/main/java/com/example/session/service/UserService.java
+++ session/src/main/java/com/example/session/service/UserService.java
@@ -9,23 +9,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    @Autowired //생성자 주입
+    @Autowired //필드 주입을 통한 의존성 주입
     private UserRepository userRepository;
 
     public void login(
             LoginRequest loginRequest,
-            HttpSession httpSession
+            HttpSession httpSession //세션 객체. 서버가 클라이언트 상태 정보를 관리하기 위해 사용.
     ){
         var id = loginRequest.getId();
         var pw = loginRequest.getPassword();
 
-        var optionalUser = userRepository.findByName(id);
+        var optionalUser = userRepository.findByName(id); //id를 기반으로 사용자를 데이터베이스에서 조회
 
         if(optionalUser.isPresent()){
             var userDto = optionalUser.get();
 
             if(userDto.getPassword().equals(pw)){
-                //세션에 정보를 저장
+                //세션에 정보를 저장 (USER라는 key로 사용자 정보 저장)
                 httpSession.setAttribute("USER", userDto);
             }else{
                 throw new RuntimeException("Password Not Match");
