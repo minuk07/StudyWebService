06e70763b4ba79053adf5918377c0d7f0230728f
minuk07 <lmw3164@naver.com>
2025-01-30 14:50:01 +0900

    Http Session을 이용하여 세션에 저장되었던 값 불러오기

--- session/src/main/java/com/example/session/controller/UserApiController.java
+++ session/src/main/java/com/example/session/controller/UserApiController.java
@@ -18,7 +18,7 @@ public class UserApiController {
         //저장해놨던 세션에서 값을 꺼내서 사용.
 
         if(userObject != null){ //세션이 만료되면 null이 리턴될 수도 있음.
-            var userDto = (UserDto) userObject; //userDto로 형변환
+            var userDto = (UserDto) userObject; //userDto로 형변환하여 userDto로 리턴.
             return userDto;
         }else{
             return null;
